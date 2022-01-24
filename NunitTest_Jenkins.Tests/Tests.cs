c04c892d98df24687cb814a7c4ab1e166d79a0fc
dhananjay kurekar <dhananjay.kurekar@Indegene.com>
2022-01-24 16:41:47 +0530

    Updates tests

--- NunitTest_Jenkins.Tests/Tests.cs
+++ NunitTest_Jenkins.Tests/Tests.cs
@@ -1,21 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using NUnit.Framework;
 using NunitTest_Jenkins.AppServices;
 
-namespace NUnitTest_Jenkins.Tests
+namespace NunitTest_Jenkins.Tests
 {
+
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void AdditionTest()
         {
             var obj = new NewAppService();
 
             var result = obj.Add(2, 3);
+
             Assert.AreEqual(5, result);
         }
     }
